23575aca4968543f23044b634cda4a90b358aea8
github username <github email address>
2017-03-15 15:30:04 -0400

    passed model specs

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,19 @@
 class Ride < ActiveRecord::Base
-  # write associations here
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+    user, attraction = User.find(user_id), Attraction.find(attraction_id)
+    message = 'Sorry. '
+    message += "You do not have enough tickets the #{attraction.name}. " if user.tickets < attraction.tickets
+    message += "You are not tall enough to ride the #{attraction.name}." if user.height < attraction.min_height
+    if message == 'Sorry. '
+      user.tickets -= attraction.tickets
+      user.happiness += attraction.happiness_rating
+      user.nausea += attraction.nausea_rating
+      user.save
+    else
+      message.strip
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
 
 
+  resources :attractions
+  resources :users
+
 end
